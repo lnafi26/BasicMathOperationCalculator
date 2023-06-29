60715c5e2423a83053ecd1d17ae8af7af0c06b24
lnafi26 <136921084+lnafi26@users.noreply.github.com>
2023-06-29 15:23:34 -0400

    Update BasicMathOperationCalculator.cpp

--- BasicMathOperationCalculator.cpp
+++ BasicMathOperationCalculator.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main(){
-    float num1, num2, numSol; //declares float vars for user-inputted terms (num1 and num2) and the solution (numSol)
-    char mathOperation; //declares char var for user-inputted operator
+    float num1, num2, sol; //declares float vars for user-inputted terms (num1 and num2) and the solution (sol)
+    char Operation; //declares char var for user-inputted operator
 
     cout << "Enter the symbol of the operation you wish to carry out: ";
-    cin >> mathOperation; //accepts user-inputted operator
+    cin >> Operation; //accepts user-inputted operator
 
-    while (!(mathOperation == ('+' || '-' || '*' || '/'))){
+    while (!(Operation == ('+' || '-' || '*' || '/'))){
         cout << "Please enter a valid mathematical operator: ";
-        cin >> mathOperation;
+        cin >> Operation;
     } //checks to see if the user input was a valid operator and doesn't let the user proceed until a valid one is inputted
 
     cout << "Enter the first number: ";
@@ -19,13 +19,13 @@ int main(){
     cout << "Enter the second number: ";
     cin >> num2; //accepts second user-inputted term
 
-    if (mathOperation == '+') numSol = num1 + num2;
-    else if (mathOperation == '-') numSol = num1 - num2;
-    else if (mathOperation == '*') numSol = num1 * num2;
-    else if (mathOperation == '/') numSol = num1 / num2;
+    if (Operation == '+') sol = num1 + num2;
+    else if (Operation == '-') sol = num1 - num2;
+    else if (Operation == '*') sol = num1 * num2;
+    else if (Operation == '/') sol = num1 / num2;
     //if statements run depending on which operator was inputted
 
-    cout << "Your solution is: " << numSol; //outputs the solution
+    cout << "Your solution is: " << sol; //outputs the solution
 
     return 0; //exits program
 }
